698538368f2517cb3008d95ac57997d9307dc59e
albedi.junior@gmail.com <albedi@gmail.com>
2020-02-12 09:58:46 -0300

    salve

---
+++ R/script.R
@@ -0,0 +1,14 @@
+#script para ler tabela limpa
+#os dados originais estao num xlsxx
+
+#?read.table
+#help(read.table)
+
+#read.table("./data/Exer_1.txt")
+#head(data)
+#str(data)
+
+#table(data$Especies)###
+
+
+
